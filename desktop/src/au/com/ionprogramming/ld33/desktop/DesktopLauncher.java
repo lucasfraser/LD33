50dd6405c31ad107984eb92ba357f5e01011d49f
Lucas Fraser <lucas@ionprogramming.com.au>
2015-08-22 13:53:41 +0930

    Initial commit

---
+++ desktop/src/au/com/ionprogramming/ld33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package au.com.ionprogramming.ld33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import au.com.ionprogramming.ld33.LD33Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD33Game(), config);
+	}
+}
